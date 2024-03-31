8b66531148b0440011e5dc782107f40e4ec20e67
abhay2132 <abhaybro768@gmail.com>
2024-03-31 11:55:40 +0530

    removed netifaces

--- sockets/settings.py
+++ sockets/settings.py
@@ -2,25 +2,6 @@
 import socket
 import subprocess
 
-def _getHOST():
-    """Retrieves the IP address of the active hotspot on Windows."""
-
-    try:
-        # Approach 1: Leverage netifaces for more reliable network interface identification
-        gateway_iface = netifaces.gateways()['default'][netifaces.AF_INET][1]
-        hotspot_addrs = netifaces.ifaddresses(gateway_iface)[netifaces.AF_INET]
-        return hotspot_addrs[0]['addr']
-
-    except KeyError:
-        try:
-            # Approach 2: Fallback to socket for compatibility
-            hostname = socket.gethostname()
-            return socket.gethostbyname(hostname)
-
-        except Exception as e:
-            print("Error retrieving IP address:", e)
-            return None
-
 def getHOTSPOT():
     output = subprocess.check_output('netsh interface ipv4 show config name="Local Area Connection* 2"', shell=True, universal_newlines=True)
     output = output.split("\n")
